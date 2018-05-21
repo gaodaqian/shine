0e9a2eb58110c104ac00c8c58ea6c4ed3865ba1c
Penpo Li <penpo.li@yaok.com>
2018-05-21 14:08:36 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市景点游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont arrow-icon">&#xe6aa;</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.header {
+    display: flex;
+    height: 0.86rem;
+    line-height: 0.86rem;
+    background: $bgColor;
+    color: #fff;
+
+    .header-left {
+        width: 0.64rem;
+        float: left;
+        .back-icon{
+            text-align :center;
+            font-size: 0.4rem;
+            font-weight:700;
+        }
+    }
+
+    .header-input {
+        flex: 1;
+        margin-top: 0.12rem;
+        margin-left: 0.2rem;
+        padding-left: .2rem;
+        height: 0.64rem;
+        line-height: 0.64rem;
+        background: #fff;
+        border-radius: 0.1rem;
+        color: #ccc;
+    }
+
+    .header-right {
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+        .arrow-icon{
+            font-size:.24rem;
+            margin-left:-.04rem;
+        }
+    }
+}
+</style>
