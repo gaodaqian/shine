8b3274c936b61f0d39ec8c3daa451449bde24882
Penpo Li <penpo.li@yaok.com>
2018-05-21 16:56:13 +0800

    icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="icons">
+        <swiper :options="swiperOption">
+             <swiper-slide v-for="(page,index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl" alt="">
+                    </div>
+                    <p class="icon-desc">{{ item.desc }}</p>
+                </div>
+             </swiper-slide>
+        </swiper>
+    </div>
+</template>
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {},
+      iconList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点名票'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc: '动植物园'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/67/9a1678221b8e0e02.png',
+          desc: '古水北镇'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '必游榜单'
+        },
+        {
+          id: '0005',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
+          desc: '踏春赏花'
+        },
+        {
+          id: '0006',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/ed/cf572be30fc32f02.png',
+          desc: '精选'
+        },
+        {
+          id: '0007',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/dd/cce1231836f10a02.png',
+          desc: '周边游'
+        },
+        {
+          id: '0008',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/b6/37560ece9c62b502.png',
+          desc: '城市观光'
+        },
+        {
+          id: '0009',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '演出'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>> .swiper-container {
+    height: 0;
+    padding-bottom: 50%;
+}
+
+.icon {
+    position: relative;
+    overflow: hidden;
+    width: 25%;
+    height: 0;
+    float: left;
+    padding-bottom: 25%;
+    background-color: #fff;
+
+    .icon-img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0.44rem;
+        box-sizing: border-box;
+        padding: 0.1rem;
+
+        .icon-img-content {
+            display: block;
+            margin: 0 auto;
+            height: 100%;
+        }
+    }
+
+    .icon-desc {
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        line-height: 0.44rem;
+        height: 0.44rem;
+        text-align: center;
+        color: $darkTextColor;
+        ellipsis()
+    }
+}
+</style>
