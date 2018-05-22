0c5bdc0363071d75b97db6a357bb3520bce3fe61
Penpo Li <penpo.li@yaok.com>
2018-05-22 16:04:08 +0800

    city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
             <span class="iconfont">&#xe632;</span>
             输入城市景点游玩主题
         </div>
-        <div class="header-right">
-            {{ this.city }}
-            <span class="iconfont arrow-icon">&#xe6aa;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{ this.city }}
+                <span class="iconfont arrow-icon">&#xe6aa;</span>
+            </div>
+        </router-link>
     </div>
 </template>
 <script>
@@ -26,8 +28,8 @@ export default {
 @import '~styles/varibles.styl'
 .header
     display: flex
-    height: 0.86rem
-    line-height: 0.86rem
+    height: $headerHeight
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -51,6 +53,7 @@ export default {
         width: 1.24rem
         float: right
         text-align: center
+        color: #fff
         .arrow-icon
             font-size: 0.24rem
             margin-left: -0.04rem
